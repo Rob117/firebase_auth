4ed122ee4b975d54a7bf16b45c74fbcb54ea195f
Robert Sherling <rob@robsherling.com>
2017-03-20 15:53:35 -0400

    Add skeleton for login form

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
+// always import our custom components at the end
 import { Header } from './components/common';
 
+
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyB6sqshitsJQHUii55QKkbnYz0Qk7BX4uA',
+      authDomain: 'blaze-dev-97082.firebaseapp.com',
+      databaseURL: 'https://blaze-dev-97082.firebaseio.com',
+      storageBucket: 'blaze-dev-97082.appspot.com',
+      messagingSenderId: '398145369198'
+    });
+  }
   render() {
     return (
       <View>
