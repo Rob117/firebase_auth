7cbb883698c4bd8d40a7ffe4e63011fdded51f44
Robert Sherling <rob@robsherling.com>
2017-03-20 18:00:04 -0400

    Add login firebase function

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,18 +1,50 @@
 import React, { Component } from 'react';
-import { TextInput } from 'react-native';
-import { Button, Card, CardSection } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, TextField } from './common';
 
 // Text inputs render with HxW of 0
+// this.setState({text: text}) -> this.setState({text})
 class LoginForm extends Component {
+  state = { email: '', password: '', error: ''};
+
+  onButtonPress() {
+    const { email, password } = this.state
+    this.setState({error: ''});
+    firebase.auth().signInWithEmailAndPassword(email, password)
+     .catch(() => {
+       firebase.auth().createUserWithEmailAndPassword(email, password)
+         .catch(() => {
+           this.setState({error: 'Authentication failed'});
+         });
+     });
+  }
+
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput style={{height: 20, width: 100}}/>
+          <TextField
+            value={this.state.email}
+            onChangeText={email => this.setState({ email })}
+            label="Email"
+            placeholder="example@gmail.com"
+          />
         </CardSection>
-        <CardSection />
         <CardSection>
-          <Button>
+          <TextField
+            value={this.state.password}
+            onChangeText={password => this.setState({ password })}
+            label="Password"
+            placeholder="password1"
+            secureTextEntry
+          />
+        </CardSection>
+        <Text style={styles.errorTextStyle}>
+          {this.state.error}
+        </Text>
+        <CardSection>
+          <Button onPress={this.onButtonPress.bind(this)}>
             Log In
           </Button>
         </CardSection>
@@ -20,5 +52,13 @@ class LoginForm extends Component {
     );
   }
 }
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
+  }
+};
 
 export default LoginForm;
